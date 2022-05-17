7997c2fc5a90352a373b33de516d8167983424dd
Brian Luong <bhluong1codes@gmail.com>
2022-05-17 04:02:17 +0000

    Completed react-elements

---
+++ react-elements/src/index.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import * as ReactDOM from 'react-dom/client';
+
+const element = React.createElement(
+  'h1',
+  [null],
+  ['Hello, React!']
+)
+
+const container = document.getElementById('root');
+
+const root = ReactDOM.createRoot(container);
+
+root.render(element);
