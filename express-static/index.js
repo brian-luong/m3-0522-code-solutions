56b0c4a23172eccdd70af3685e8b9c70ab270f6b
Brian Luong <bhluong1codes@gmail.com>
2022-06-10 01:42:47 +0000

    Updated express-static

--- express-static/index.js
+++ express-static/index.js
@@ -1,15 +1,16 @@
 
 const express = require('express');
-const app = express();
+
 const path = require('path');
+
+const app = express();
+
+const absolutePath = path.join(__dirname, 'public')
+const public = express.static(absolutePath);
+app.use(public);
 
 
 app.listen(3000, () => {
   // eslint-disable-next-line no-console
   console.log('Express server listening on port 3000');
 });
-
-const absolutePath = path.join(__dirname, 'public')
-
-const public = express.static('public');
-app.use(public);
