5282e61a678357761364b1896abd6411cf99df68
Brian Luong <bhluong1codes@gmail.com>
2022-06-13 06:01:24 +0000

    Skeleton

---
+++ react-carousel-component/src/carousel.jsx
@@ -0,0 +1,28 @@
+import React from "react";
+
+
+class Carousel extends React.Component {
+  render(){
+
+    return (
+      <div className="carousel">
+        <div className="image-carousel">
+          <i className="fas fa-chevron-left ctrl"></i>
+          <div className="img-gallery">
+            <img src="https://assets.pokemon.com/static2/_ui/img/og-default-image.jpeg" alt=""/>
+          </div>
+          <i className="fas fa-chevron-right ctrl"></i>
+        </div>
+        <div className="progress-bar">
+          <div className="progress-dot"><i className="fas fa-circle" data-id="1"></i></div>
+          <div className="progress-dot"><i className="far fa-circle" data-id="2"></i></div>
+          <div className="progress-dot"><i className="far fa-circle" data-id="3"></i></div>
+          <div className="progress-dot"><i className="far fa-circle" data-id="4"></i></div>
+          <div className="progress-dot"><i className="far fa-circle" data-id="5"></i></div>
+        </div>
+      </div>
+    )
+  }
+}
+
+export default Carousel;
