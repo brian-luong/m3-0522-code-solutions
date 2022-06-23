4e881af91c1a590c61549551d80e21c60d39e6bc
Brian Luong <bhluong1codes@gmail.com>
2022-06-23 05:02:53 +0000

    Add in constructor and state

--- react-carousel-component/src/carousel.jsx
+++ react-carousel-component/src/carousel.jsx
@@ -2,6 +2,12 @@ import React from "react";
 
 
 class Carousel extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      activeId: 0
+    };
+  }
   render(){
 
     return (
--- react-carousel-component/src/index.jsx
+++ react-carousel-component/src/index.jsx
@@ -5,4 +5,23 @@ import Carousel from "./carousel";
 const container = document.getElementById('root');
 const root = ReactDOM.createRoot(container);
 
+const data = [
+  {
+  pokemonId: 1,
+    imageUrl: 'https://assets.pokemon.com/static2/_ui/img/og-default-image.jpeg'
+  },
+  {
+    pokemonId: 2,
+    imageUrl: 'https://assets.pokemon.com/static2/_ui/img/og-default-image.jpeg'
+  },
+  {
+    pokemonId: 3,
+    imageUrl: 'https://assets.pokemon.com/static2/_ui/img/og-default-image.jpeg'
+  },
+  {
+    pokemonId: 4,
+    imageUrl: 'https://assets.pokemon.com/static2/_ui/img/og-default-image.jpeg'
+  }
+]
+
 root.render(<Carousel/>);
